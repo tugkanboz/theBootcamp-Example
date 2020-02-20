a78c3da38ef3b55f38ff142c305718815204bbf9
decello <tgkn.boz@gmail.com>
2020-02-20 14:49:21 +0300

    EJS Demo.

---
+++ SampleNode.js/EJSDemo/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+let posts = [
+    {title: "Post 1", author: "Tugkan"},
+    {title: "Post 2", author: "Tugo"},
+    {title: "Post 3", author: "Tug"}
+];
+
+let love = [
+    "love","lovee","loveee","loveeee"
+];
+
+app.get("/",function (req,res) {
+    res.render("home");
+});
+
+app.get("/fallinlovewith/:thing",function (req,res) {
+    let thing = req.params.thing;
+    res.render("love",{thingVar:thing});
+});
+
+app.get("/posts",function (req,res) {
+    res.render("posts", {posts:posts, love:love});
+});
+
+app.post("/addLove", function (req, res) {
+    let newLove = req.body.newLove;
+    love.push(newLove);
+    res.redirect("/posts");
+});
+
+
+//localhost way
+app.listen(3000, function() {
+    console.log('Server listening on port 3000');
+});
