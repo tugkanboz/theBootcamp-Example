86ce45d4301f0ed5dc2c342958fb0ccac063313b
Tuğkan Boz <tgkn.boz@gmail.com>
2021-07-07 20:37:19 +0300

    test

--- Authentication/app.js
+++ Authentication/app.js
@@ -20,7 +20,6 @@ app.use(require('express-session')({
   resave: false
 }));
 
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
